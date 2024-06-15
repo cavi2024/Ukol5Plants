aaed8d777f05a15d2bb3eafddc324cefa42ca752
Gabriela Horčičková <cavilliera@centrum.cz>
2024-06-15 12:13:29 +0200

    Třída PlantException
    Ošetření zadávání frekvence zálivky - kladné číslo

--- src/plants/PlantException.java
+++ src/plants/PlantException.java
@@ -1,7 +1,15 @@
 package plants;
 
+import java.time.LocalDate;
+
 public class PlantException extends Exception{
     public PlantException(String message) {
     super(message);
+    }
+
+    public static void validateWateringFrequency(int dayFrequencyOfWatering) throws PlantException {
+        if (dayFrequencyOfWatering <= 0) {
+            throw new PlantException("Frekvence zálivky musí být kladné číslo.");
+        }
     }
 }
