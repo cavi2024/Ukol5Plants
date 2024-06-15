c55c1156e6d1415a7f5d6691fa01bf7c88df044c
Gabriela Horčičková <cavilliera@centrum.cz>
2024-06-15 13:47:19 +0200

    Třída ListPlants
    Vytvoření třídy se seznamem

---
+++ src/plants/ListPlants.java
@@ -0,0 +1,12 @@
+package plants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListPlants {
+    private List<Plant> plants;
+
+    public ListPlants() {
+        plants = new ArrayList<>();
+    }
+}
