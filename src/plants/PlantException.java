0d7699bf8eb22691b1369872b4014578bd67c308
Gabriela Horčičková <cavilliera@centrum.cz>
2024-06-15 12:14:32 +0200

    Třída PlantException
    Ošetření zadávání data poslední zálivky - nesmí být starší než datum zasazení rostliny.

--- src/plants/PlantException.java
+++ src/plants/PlantException.java
@@ -12,4 +12,10 @@ public class PlantException extends Exception{
             throw new PlantException("Frekvence zálivky musí být kladné číslo.");
         }
     }
+
+    public static void validateLastWateringDate(LocalDate dateOfPlanted, LocalDate dateOfLastWatering) throws PlantException {
+        if (dateOfLastWatering.isBefore(dateOfPlanted)) {
+            throw new PlantException("Datum poslední zálivky nesmí být starší než datum zasazení rostliny.");
+        }
+    }
 }
