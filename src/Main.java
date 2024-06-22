027382643d6909602e4c6121560b4c78f2074d66
Gabriela Horčičková <cavilliera@centrum.cz>
2024-06-22 19:28:30 +0200

    Třída Main
    Řazení podle názvu květiny a výpis

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import plants.PlantException;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
@@ -60,5 +61,10 @@ public class Main {
         } catch (PlantException e) {
             System.err.println(e.getMessage());
         }
+
+        // Řazení podle názvu květiny a výpis:
+        Collections.sort(newListPlants.getPlants());
+        System.out.println("Seřazeno podle názvu:");
+        newListPlants.printWateringInfo();
     }
 }
