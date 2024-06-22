85783130ae9e0d19d4c2406fb2a1bc57b147dde6
Gabriela Horčičková <cavilliera@centrum.cz>
2024-06-22 17:29:29 +0200

    Třída Main
    Doplnění try-catch do importu a exportu

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,54 @@
 import plants.ListPlants;
 import plants.Plant;
+import plants.PlantException;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         ListPlants listPlants = new ListPlants();
+
+        // načtení ze souboru kvetiny.txt:
         try {
-            // načtení ze souboru kvetiny.txt:
-            listPlants.importFromFile("kvetiny.txt");
+            listPlants.importFromFile("kvetiny.txt", "\t");
+        } catch (IOException e) {
+            System.err.println("Chyba při načtení souboru: " + e.getMessage());
+        } catch (PlantException e) {
+            System.err.println(e.getMessage());
+        }
 
-            // Výpis informací o zálivce:
-            listPlants.printWateringInfo();
+        // Výpis informací o zálivce:
+        listPlants.printWateringInfo();
 
-            // Přidání dvou květin:
-            Plant newPlant1 = new Plant("Bazalka v kuchyni", LocalDate.of(2021, 9, 4),
-                    3, LocalDate.of(2021, 9, 4), " ");
-            Plant newPlant2 = new Plant("Růže", LocalDate.of(2022, 3, 15),
-                    5, LocalDate.of(2022, 3, 10), "červená");
-            listPlants.addPlant(newPlant1);
-            listPlants.addPlant(newPlant2);
+        // Přidání dvou květin:
+        Plant newPlant1 = new Plant("Bazalka v kuchyni", LocalDate.of(2021, 9, 4),
+                3, LocalDate.of(2021, 9, 4), " ");
+        Plant newPlant2 = new Plant("Růže", LocalDate.of(2022, 3, 15),
+                5, LocalDate.of(2022, 3, 10), "červená");
+        listPlants.addPlant(newPlant1);
+        listPlants.addPlant(newPlant2);
 
-            // Odebrání druhé květiny:
-            listPlants.removePlant(1);
+        // Odebrání druhé květiny:
+        listPlants.removePlant(1);
 
-            // Uložení do nového souboru:
-            listPlants.exportToFile("novy_seznam_kvetin.txt");
+        // Uložení do nového souboru:
+        try {
+            listPlants.exportToFile("novy_seznam_kvetin.txt", "\t");
+        } catch (IOException e) {
+            System.err.println("Chyba při exportu souboru: " + e.getMessage());
+        } catch (PlantException e) {
+            System.err.println(e.getMessage());
+        }
 
+        // Opětovné načtení vygenerovaného souboru:
+        ListPlants newListPlants = new ListPlants();
+        try {
+            newListPlants.importFromFile("novy_seznam_kvetin.txt", "\t");
+        } catch (IOException e) {
+            System.err.println("Chyba při opětovném načtení souboru: " + e.getMessage());
+        } catch (PlantException e) {
+            System.err.println(e.getMessage());
         }
     }
 }
