ce34ffd92b020fea65274e82a1a0c1978ae908e5
Gabriela Horčičková <cavilliera@centrum.cz>
2024-06-22 15:59:28 +0200

    Třída Main
    Přidání dvou květin

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
 import plants.ListPlants;
+import plants.Plant;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         ListPlants listPlants = new ListPlants();
         try {
-           // načtení ze souboru kvetiny.txt:
-           listPlants.importFromFile("kvetiny.txt");
+            // načtení ze souboru kvetiny.txt:
+            listPlants.importFromFile("kvetiny.txt");
 
-           // Výpis informací o zálivce:
-           listPlants.printWateringInfo();
+            // Výpis informací o zálivce:
+            listPlants.printWateringInfo();
+
+            // Přidání dvou květin:
+            Plant newPlant1 = new Plant("Bazalka v kuchyni", LocalDate.of(2021, 9, 4),
+                    3, LocalDate.of(2021, 9, 4), " ");
+            Plant newPlant2 = new Plant("Růže", LocalDate.of(2022, 3, 15),
+                    5, LocalDate.of(2022, 3, 10), "červená");
+            listPlants.addPlant(newPlant1);
+            listPlants.addPlant(newPlant2);
+
+
+
         }
     }
 }
